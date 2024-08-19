7172e1fbee66e526b6a1c2715edb8eb57e8a0426
bohdan <mota82589@gmail.com>
2024-08-19 13:09:40 +0300

    header styled by Moya

--- rental/src/App.tsx
+++ rental/src/App.tsx
@@ -1,11 +1,21 @@
 import "./App.css";
 import React from "react";
 import CarCard from "./components/carCard/carCard";
+import Header from "./components/Header/header";
+
 const App: React.FC = () => {
   return (
-    <>
+    <div
+      style={{
+        border: "1px solid black",
+        width: "100%",
+        maxWidth: "1440px",
+        margin: "0 auto",
+      }} //do not do it like this better declare a class :)
+    >
+      <Header></Header>
       <CarCard />
-    </>
+    </div>
   );
 };
 
--- rental/src/components/Header/header.tsx
+++ rental/src/components/Header/header.tsx
@@ -1,5 +1,26 @@
+import style from "./header.module.css";
+
 const Header: React.FC = () => {
-  return <></>;
+  return (
+    <div className={style.heder}>
+      <p
+        className={style.targeMrental}
+        style={{ color: "#c59900", fontSize: "24px", fontWeight: "bold" }}
+      >
+        target<span style={{ color: "yellow" }}>M</span>rental
+      </p>
+      <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+        <div className={style.list} style={{ display: "flex", gap: "10px" }}>
+          <button>Автопарк</button>
+          <button>Про нас</button>
+          <button>Умови прокату</button>
+        </div>
+        <div>
+          <button className={style.button}>Орендувати</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
